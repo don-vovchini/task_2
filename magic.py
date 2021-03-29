da335d244ff489021cc0464705cb668127277c49
Vladimir <zavaruhin-425@yandex.ru>
2021-03-29 23:15:20 +0500

    Магическая спираль

--- magic.py
+++ magic.py
@@ -1,3 +1,5 @@
+from typing import List
+
 print("Для завершения работы отправьте пустую строку")
 n = input("Введите число не меньше 4 и не больше 1000: ")
 while len(n) != 0:
@@ -7,11 +9,16 @@ while len(n) != 0:
         n = input("Введите число не меньше 4 и не больше 1000: ")
     else:
         mat = [[0]*n for i in range(n)]
-        l = 0
+        m = 1
         if n % 2 > 0:
-            mat[n//2][n//2] = n ** 2
+            mat[n // 2][n // 2] = n ** 2
         for i in range(n // 2):
-            print(i)
+            for k in range(n - 2 * i - 1):
+                mat[i][k + i] = m + k
+                mat[k + i][n - 1 - i] = m + k + n - 2 * i - 1
+                mat[n - 1 - i][n - 1 - i - k] = m + k + 2 * (n - 2 * i - 1)
+                mat[n - 1 - i - k][i] = m + k + 3 * (n - 2 * i - 1)
+            m = m + 4 * (n - (1 + 2 * i))
         for i in mat:
             print(*i)
         print("Для завершения работы отправьте пустую строку")
