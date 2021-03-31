909aa1b27876fb3e202eeb8c88273bbf65acd880
Vladimir <zavaruhin-425@yandex.ru>
2021-03-31 15:19:25 +0500

    Магическая спираль

--- magic.py
+++ magic.py
@@ -21,6 +21,6 @@ while len(n) != 0:
                 mat[n - 1 - i - k][i] = m + k + 3 * (n - 2 * i - 1)
             m = m + 4 * (n - (1 + 2 * i))
         for i in mat:
-            print(*i)
+            print(' '.join(['{:{l}}' for a in range(n)]).format(*i, l=len(str(n * n))))
         print("Для завершения работы отправьте пустую строку")
         n = input("Введите число не меньше 4 и не больше 1000: ")
